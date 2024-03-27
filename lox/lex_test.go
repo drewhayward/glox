d7ab69cdab0e447af1ed47acb7fe17186af2a809
Drew Hayward <d.andrew.hayward@gmail.com>
2024-03-26 20:16:54 -0400

    try snapshot testing

--- lox/lex_test.go
+++ lox/lex_test.go
@@ -2,72 +2,63 @@ package lox
 
 import (
 	"fmt"
+	"github.com/gkampitakis/go-snaps/snaps"
 	"testing"
 )
 
-func TestScanTokens(t *testing.T) {
+func TestLexSnapshot(t *testing.T) {
 	testCases := []struct {
-		source   string
-		expected []TokenType
+		source string
 	}{
-		{"()", []TokenType{LEFT_PAREN, RIGHT_PAREN}},
-		{"{}", []TokenType{LEFT_BRACE, RIGHT_BRACE}},
-		{"/", []TokenType{SLASH}},
-		{".", []TokenType{DOT}},
-		{",", []TokenType{COMMA}},
-		{"-", []TokenType{MINUS}},
-		{"+", []TokenType{PLUS}},
-		{";", []TokenType{SEMICOLON}},
-		{"/", []TokenType{SLASH}},
-		{"*", []TokenType{STAR}},
-		{"!", []TokenType{BANG}},
-		{"!=", []TokenType{BANG_EQUAL}},
-		{"=", []TokenType{EQUAL}},
-		{"==", []TokenType{EQUAL_EQUAL}},
-		{">=", []TokenType{GREATER_EQUAL}},
-		{">", []TokenType{GREATER}},
-		{"<", []TokenType{LESS}},
-		{"<=", []TokenType{LESS_EQUAL}},
-		{"\"testing\"", []TokenType{STRING}},
-		{"123", []TokenType{NUMBER}},
-		{"123.", []TokenType{NUMBER, DOT}},
-		{"123.09", []TokenType{NUMBER}},
-		{"testing", []TokenType{IDENTIFIER}},
-		{"for", []TokenType{FOR}},
-		{"and", []TokenType{AND}},
-		{"class", []TokenType{CLASS}},
-		{"else", []TokenType{ELSE}},
-		{"false", []TokenType{FALSE}},
-		{"fun", []TokenType{FUN}},
-		{"for", []TokenType{FOR}},
-		{"if", []TokenType{IF}},
-		{"nil", []TokenType{NIL}},
-		{"or", []TokenType{OR}},
-		{"print", []TokenType{PRINT}},
-		{"return", []TokenType{RETURN}},
-		{"super", []TokenType{SUPER}},
-		{"this", []TokenType{THIS}},
-		{"true", []TokenType{TRUE}},
-		{"var", []TokenType{VAR}},
-		{"while", []TokenType{WHILE}},
-		{"var test = \"foobar\"", []TokenType{VAR, IDENTIFIER, EQUAL, STRING}},
-		{"var\nvar", []TokenType{VAR, VAR}},
+		{"()"},
+		{"{}"},
+		{"/"},
+		{"."},
+		{","},
+		{"-"},
+		{"+"},
+		{";"},
+		{"/"},
+		{"*"},
+		{"!"},
+		{"!="},
+		{"="},
+		{"=="},
+		{">="},
+		{">"},
+		{"<"},
+		{"<="},
+		{"\"testing\""},
+		{"123"},
+		{"123."},
+		{"123.09"},
+		{"testing"},
+		{"for"},
+		{"and"},
+		{"class"},
+		{"else"},
+		{"false"},
+		{"fun"},
+		{"for"},
+		{"if"},
+		{"nil"},
+		{"or"},
+		{"print"},
+		{"return"},
+		{"super"},
+		{"this"},
+		{"true"},
+		{"var"},
+		{"while "},
+		{"var test = \"foobar\""},
+		{"var\nvar"},
 		// Add more test cases as needed
 	}
 
 	for _, tc := range testCases {
 		t.Run(fmt.Sprintf("ScanTokens(%q)", tc.source), func(t *testing.T) {
 			tokens, _ := ScanTokens(tc.source)
-			if len(tokens) != len(tc.expected) {
-				t.Errorf("ScanTokens(%q) returned %d tokens, expected %d tokens", tc.source, len(tokens), len(tc.expected))
-				return
-			}
-
-			for i, token := range tokens {
-				if token.type_ != tc.expected[i] {
-					t.Errorf("ScanTokens(%q) is wrong: got %s, expected %s", tc.source, token, tc.expected[i])
-				}
-			}
+			snaps.MatchSnapshot(t, tokens)
 		})
 	}
 }
--- lox/parse_test.go
+++ lox/parse_test.go
@@ -1,108 +1,30 @@
 package lox
 
-// func TestParseTokens(t *testing.T) {
-// 	testCases := []struct {
-// 		input      []Token
-// 		expression Expr
-// 	}{
-// 		// 1
-// 		{[]Token{{NUMBER, "1", "1", 0}}, Literal[string]{"1"}},
-// 		// "aoeu"
-// 		{[]Token{{STRING, "aoeu", "aoeu", 0}}, Literal[string]{"aoeu"}},
-// 		// nil
-// 		{[]Token{{NIL, "nil", "nil", 0}}, Literal[*struct{}]{nil}},
-// 		// ("aoeu")
-// 		{[]Token{
-// 			{LEFT_PAREN, "(", "(", 0},
-// 			{STRING, "aoeu", "aoeu", 0},
-// 			{RIGHT_PAREN, ")", ")", 0},
-// 		}, Literal[string]{"aoeu"}},
-// 		// (1)
-// 		{[]Token{
-// 			{LEFT_PAREN, "(", "(", 0},
-// 			{NUMBER, "1", "1", 0},
-// 			{RIGHT_PAREN, ")", ")", 0},
-// 		}, Literal[string]{"1"}},
-// 		// -1
-// 		{[]Token{
-// 			{MINUS, "-", "-", 0},
-// 			{NUMBER, "1", "1", 0},
-// 		}, UnaryExpr{MINUS, Literal[string]{"1"}}},
-// 		// !1
-// 		{[]Token{
-// 			{BANG, "!", "!", 0},
-// 			{NUMBER, "1", "1", 0},
-// 		}, UnaryExpr{BANG, Literal[string]{"1"}}},
-// 		// 1 * 1
-// 		{[]Token{
-// 			{NUMBER, "1", "1", 0},
-// 			{STAR, "*", "*", 0},
-// 			{NUMBER, "1", "1", 0},
-// 		}, BinaryExpr{STAR, Literal[string]{"1"}, Literal[string]{"1"}}},
-// 		// 1 / 1
-// 		{[]Token{
-// 			{NUMBER, "1", "1", 0},
-// 			{SLASH, "/", "/", 0},
-// 			{NUMBER, "1", "1", 0},
-// 		}, BinaryExpr{SLASH, Literal[string]{"1"}, Literal[string]{"1"}}},
-// 		// 1 + 1
-// 		{[]Token{
-// 			{NUMBER, "1", "1", 0},
-// 			{PLUS, "+", "+", 0},
-// 			{NUMBER, "1", "1", 0},
-// 		}, BinaryExpr{PLUS, Literal[string]{"1"}, Literal[string]{"1"}}},
-// 		// 1 - 1
-// 		{[]Token{
-// 			{NUMBER, "1", "1", 0},
-// 			{MINUS, "-", "-", 0},
-// 			{NUMBER, "1", "1", 0},
-// 		}, BinaryExpr{MINUS, Literal[string]{"1"}, Literal[string]{"1"}}},
-// 		// 1 < 1
-// 		{[]Token{
-// 			{NUMBER, "1", "1", 0},
-// 			{LESS, "*", "*", 0},
-// 			{NUMBER, "1", "1", 0},
-// 		}, BinaryExpr{LESS, Literal[string]{"1"}, Literal[string]{"1"}}},
-// 		// 1 <= 1
-// 		{[]Token{
-// 			{NUMBER, "1", "1", 0},
-// 			{LESS_EQUAL, "<=", "<=", 0},
-// 			{NUMBER, "1", "1", 0},
-// 		}, BinaryExpr{LESS_EQUAL, Literal[string]{"1"}, Literal[string]{"1"}}},
-// 		// 1 > 1
-// 		{[]Token{
-// 			{NUMBER, "1", "1", 0},
-// 			{GREATER, ">", ">", 0},
-// 			{NUMBER, "1", "1", 0},
-// 		}, BinaryExpr{GREATER, Literal[string]{"1"}, Literal[string]{"1"}}},
-// 		// 1 >= 1
-// 		{[]Token{
-// 			{NUMBER, "1", "1", 0},
-// 			{GREATER_EQUAL, ">=", ">=", 0},
-// 			{NUMBER, "1", "1", 0},
-// 		}, BinaryExpr{GREATER_EQUAL, Literal[string]{"1"}, Literal[string]{"1"}}},
-// 		// 1 == 1
-// 		{[]Token{
-// 			{NUMBER, "1", "1", 0},
-// 			{EQUAL_EQUAL, "==", "==", 0},
-// 			{NUMBER, "1", "1", 0},
-// 		}, BinaryExpr{EQUAL_EQUAL, Literal[string]{"1"}, Literal[string]{"1"}}},
-// 		// 1 + 1 + 1
-// 		{[]Token{
-// 			{NUMBER, "1", "1", 0},
-// 			{PLUS, "+", "+", 0},
-// 			{NUMBER, "1", "1", 0},
-// 			{PLUS, "+", "+", 0},
-// 			{NUMBER, "1", "1", 0},
-// 		}, BinaryExpr{PLUS, BinaryExpr{PLUS, Literal[string]{"1"}, Literal[string]{"1"}}, Literal[string]{"1"}}},
-// 	}
-//
-// 	for _, tc := range testCases {
-// 		t.Run(fmt.Sprintf("ParseTokens(%q)", tc.input), func(t *testing.T) {
-// 			result := ParseExpr(tc.input)
-// 			if result != tc.expression {
-// 				t.Errorf("ParseTokens(%q) returned the wrong ast. Got %q, wanted: %q", tc.input, result, tc.expression)
-// 			}
-// 		})
-// 	}
-// }
+import (
+	"fmt"
+	"github.com/gkampitakis/go-snaps/snaps"
+	"testing"
+)
+
+func TestParseSnapshot(t *testing.T) {
+	testCases := []struct {
+		source string
+	}{
+		{`
+            var a = 1;
+        `},
+	}
+
+	for _, tc := range testCases {
+		s := snaps.WithConfig()
+		t.Run(fmt.Sprintf("Parse(%q)", tc.source), func(t *testing.T) {
+			tokens, _ := ScanTokens(tc.source)
+			node, err := Parse(tokens)
+			if err != nil {
+				t.Fatalf(err.Error())
+			}
+
+			s.MatchSnapshot(t, node)
+		})
+	}
+}
